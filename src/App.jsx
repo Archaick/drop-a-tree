100a37819157ee72c4b057fb13b5e8ec45f90fc3
Archaick <saeed.jh3@gmail.com>
2024-06-25 16:42:45 +0700

    fixed infinite panning and map repeat

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth'
-import ProtectedRoute from './components/ProtectedRoute'
-import { auth } from './firebase'
-
-import Home from './components/Home'
-import Private from './components/Private'
-import { useEffect, useState } from 'react'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LiveMap from './components/LiveMap';
 
 function App() {
-  const [user, setUser] = useState(null)
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user)
-        return
-      }
-      setUser(null)
-    })
-    return () => unsubscribe()
-  }, [])
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path='/' element={<Home />}></Route>
-        <Route index path='/private' 
-        element={
-        <ProtectedRoute user={user}>
-          <Private></Private>
-        </ProtectedRoute>
-      }>
-
-      </Route>
+        <Route path="/" element={<LiveMap />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LiveMap.jsx
+++ src/components/LiveMap.jsx
@@ -1,13 +1,43 @@
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const LiveMap = () => {
-    return (
-        <div>
-        
-        </div>
-    )
-}
+  const [markers, setMarkers] = useState([]);
 
-export default LiveMap
+  const MapEvents = () => {
+    useMapEvents({
+      click(e) {
+        const newMarker = { lat: e.latlng.lat, lng: e.latlng.lng };
+        setMarkers([...markers, newMarker]);
+      }
+    });
+    return null;
+  };
+
+  return (
+    <div>
+      <MapContainer
+        style={{ height: "480px", width: "100%" }}
+        center={[10, 0]}  // Center the map at a more balanced initial position
+        zoom={2}          // Set a reasonable initial zoom level
+        minZoom={2}       // Set minimum zoom level to show entire continents
+        maxZoom={18}      // Set maximum zoom level for more detail
+        maxBounds={[[85, -180], [-85, 180]]} // Restrict panning to the near-world bounds
+        maxBoundsViscosity={1.0} // Make the map less likely to be dragged outside the bounds
+        worldCopyJump={false} // Prevent the map from repeating
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={[marker.lat, marker.lng]} />
+        ))}
+        <MapEvents />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LiveMap;
