8a766596b410932911e1f81e182f945f85d0f0c4
Archaick <saeed.jh3@gmail.com>
2024-06-29 17:02:10 +0700

    add option to remove pinned areas

--- src/components/LiveMap.jsx
+++ src/components/LiveMap.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const LiveMap = () => {
@@ -15,24 +15,34 @@ const LiveMap = () => {
     return null;
   };
 
+  const removeMarker = (indexToRemove) => {
+    setMarkers(markers.filter((_, index) => index !== indexToRemove));
+  };
+
   return (
     <div>
       <MapContainer
         style={{ height: "480px", width: "100%" }}
         center={[10, 25]}  // Center the map at a more balanced initial position
-        zoom={3}          // Set a reasonable initial zoom level
-        minZoom={3}       // Set minimum zoom level to show entire continents
-        maxZoom={18}      // Set maximum zoom level for more detail
+        zoom={3}           // Set a reasonable initial zoom level
+        minZoom={3}        // Set minimum zoom level to show entire continents
+        maxZoom={18}       // Set maximum zoom level for more detail
         maxBounds={[[85, -180], [-85, 180]]} // Restrict panning to the near-world bounds
         maxBoundsViscosity={1.0} // Make the map less likely to be dragged outside the bounds
-        worldCopyJump={false} // Prevent the map from repeating
+        worldCopyJump={false}    // Prevent the map from repeating
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
         {markers.map((marker, index) => (
-          <Marker key={index} position={[marker.lat, marker.lng]} />
+          <Marker key={index} position={[marker.lat, marker.lng]}>
+            <Popup>
+              <div>
+                <button onClick={() => removeMarker(index)}>Remove</button>
+              </div>
+            </Popup>
+          </Marker>
         ))}
         <MapEvents />
       </MapContainer>
