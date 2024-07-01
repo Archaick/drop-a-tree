16d74e5f66b48a077b405a0b115eb67df5a287c8
Archaick <saeed.jh3@gmail.com>
2024-07-01 19:04:25 +0700

    Auth registeration and log in works

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import LiveMap from './components/LiveMap';
 import SignUp from './components/SignUp';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import Auth from './components/Auth';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Live Map</Link>
-        <Link to="/signup">Sign Up</Link>
-      </nav>
-      <Routes>
-        <Route path='/signup' element={<SignUp />}/>
-        <Route path="/" element={<LiveMap />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <nav>
+    //     <Link to="/">Live Map</Link>
+    //     <Link to="/signup">Sign Up</Link>
+    //   </nav>
+    //   <Routes>
+    //     <Route path='/signup' element={<SignUp />}/>
+    //     <Route path="/" element={<LiveMap />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+        <Auth />
+        <LiveMap />
+    </div>
+
   );
 }
 
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,14 +3,36 @@ import { auth } from '../firebase'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 const Auth = () => {
-    emailRef = useRef()
-    passwordRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
     const [error, setError] = useState(null)
     const [isSignUp, setIsSignUp] = useState(true)
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            if (isSignUp) {
+                await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+            } else {
+                await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+            }
+        } catch (err) {
+            setError(err.message)
+            console.log(error)
+        }
+    }
+
     return (
         <div>
-        
+            <h2>{isSignUp ? 'Register' : 'Log In'}</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="email" ref={emailRef} placeholder='Email' required/>
+                <input type="password" ref={passwordRef} placeholder='Password' required/>
+                {error && <p>{error}</p>}
+                <button type='submit' onClick={() => setIsSignUp(!isSignUp)}> 
+                    {isSignUp ? 'Register' : 'Log In'}
+                </button>
+            </form>
         </div>
     )
 }
