ecfe45846917d991716788a29a3367b9e4cc2427
Archaick <saeed.jh3@gmail.com>
2024-07-01 09:12:22 +0700

    Sign up componenet made

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,3 +2,29 @@ import React, { useRef, useState } from 'react'
 import { auth } from './firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 
+const SignUp = () => {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    cost [error, setError] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+        } catch (error) {
+            setError(error.massage)
+            console.error(error)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="email" ref={emailRef} placeholder='Email' required />
+            <input type="password" ref={passwordRef} placeholder='Password' required />
+            {error && <p>{error}</p>}
+            <button type='submit'>Sign Up</button>
+        </form>
+    )
+}
+
+export default SignUp
